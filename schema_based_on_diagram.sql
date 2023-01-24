ee3210ae71915eacb627089c9003bd5bb3bfcaa7
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-01-24 14:48:20 +0100

    update file with schema

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -21,3 +21,20 @@ CREATE TABLE medical_histories_treatments (
     medical_histories_id INTEGER REFERENCES medical_histories(id) ON DELETE CASCADE ON UPDATE CASCADE,
     treatments_id INTEGER REFERENCES treatments(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+CREATE TABLE invoices(
+    id SERIAL PRIMARY KEY,
+    total_amount DECIMAL NOT NULL,
+    generated_at TIMESTAMP NOT NULL,
+    payed_at TIMESTAMP NOT NULL,
+    medical_history_id INT REFERENCES medical_histories(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE invoice_items(
+    id SERIAL PRIMARY KEY,
+    unit_price DECIMAL NOT NULL,
+    quantity INT NOT NULL,
+    total_price DECIMAL NOT NULL,
+    invoice_id INT REFERENCES invoices(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    treatment_id INT REFERENCES treatments(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
