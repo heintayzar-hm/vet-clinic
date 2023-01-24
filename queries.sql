73204915119d2d5aaa07389c8736af89ee060332
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-01-24 10:43:19 +0100

    Add additional queries to file

--- queries.sql
+++ queries.sql
@@ -267,3 +267,8 @@ LIMIT 1
 explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;
 explain analyze SELECT * FROM visits where vet_id = 2;
 explain analyze SELECT * FROM owners where email = 'owner_18327@mail.com';
+
+
+SELECT COUNT(*) FROM visits where animal_id = 4;
+SELECT * FROM visits where vet_id = 2;
+SELECT * FROM owners where email = 'owner_18327@mail.com';
